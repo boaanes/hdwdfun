37f53a69e65428dde438dbacd6b8a1aaa4ed7fcf
Bo Aanes <boaanes@gmail.com>
2022-10-17 13:24:04 +0200

    yessir

--- app/Main.hs
+++ app/Main.hs
@@ -7,17 +7,18 @@ main = return ()
 exampleGraphAdjList :: [(Int, [Int])]
 exampleGraphAdjList = [(1, [7,9,8]), (2, [6]), (3, [8, 7, 5]), (4, [5, 9]), (5, [1]), (6, [1, 3]), (7, [2]), (8, [4]), (9, [3])]
 
-exapmleGraph :: Graph Int
-exapmleGraph = Overlay (Connect (Vertex 2) (Vertex 4)) (Overlay (Connect (Vertex 1) (Vertex 2)) (Connect (Vertex 2) (Vertex 3)))
+exampleGraph :: Graph Int
+exampleGraph = stars exampleGraphAdjList
 
 -- A function to get vertices pointing to a vertex
-getInboundVertices :: Int -> Graph Int -> [Int]
-getInboundVertices v g = filter (\x -> v `elem` neighbourhood x g) (vertexList g)
+inboundVertices :: Ord a =>a -> Graph a -> [a]
+inboundVertices v g = filter (\x -> v `elem` outboundVertices x g) (vertexList g)
 
 -- A function to get neighbourhood of a vertex
-neighbourhood :: Int -> Graph Int -> [Int]
-neighbourhood v g = [x | x <- vertexList g, x /= v, hasEdge v x g]
+outboundVertices :: Ord a => a -> Graph a -> [a]
+outboundVertices v g = [x | x <- vertexList g, x /= v, hasEdge v x g]
 
 -- A function to get complete neighbourhood of a vertex
-completeNeighbourhood :: Int -> Graph Int -> [Int]
+completeNeighbourhood :: Ord a => a -> Graph a -> [a]
 completeNeighbourhood v g = [x | x <- vertexList g, x /= v, hasEdge v x g || hasEdge x v g]
+
