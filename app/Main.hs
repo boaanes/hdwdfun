eaa13c55e168398ba67fd7cbf7f9ac92f6c708a8
Bo Aanes <boaanes@gmail.com>
2022-11-21 10:39:56 +0100

    Move graph helpers

--- app/Main.hs
+++ app/Main.hs
@@ -8,38 +8,6 @@ import           HotDrink
 main :: IO ()
 main = return ()
 
--------- Generic functions --------
-
--- A function to get vertices pointing to a vertex
-inboundVertices :: Ord a => a -> Graph a -> [a]
-inboundVertices v g = filter (\x -> v `elem` outboundVertices x g) (vertexList g)
-
--- A function to get neighbourhood of a vertex
-outboundVertices :: Ord a => a -> Graph a -> [a]
-outboundVertices v g = [x | x <- vertexList g, x /= v, hasEdge v x g]
-
--- A function to get complete neighbourhood of a vertex
-completeNeighbourhood :: Ord a => a -> Graph a -> [a]
-completeNeighbourhood v g = [x | x <- vertexList g, x /= v, hasEdge v x g || hasEdge x v g]
-
--- Get all vertices with no incoming edges
-getSources :: Ord a => Graph a -> [a]
-getSources g = filter (\x -> null (inboundVertices x g)) (vertexList g)
-
--- check if vertex is part of graph
-isVertexInGraph :: Ord a => a -> Graph a -> Bool
-isVertexInGraph v g = v `elem` vertexList g
-
--- Topological sort of a graph
-topologicalSort :: Ord a => Graph a -> [a]
-topologicalSort g = topologicalSort' g (getSources g) []
-
-topologicalSort' :: Ord a => Graph a -> [a] -> [a] -> [a]
-topologicalSort' _ [] sorted = sorted
-topologicalSort' g (v:vs) sorted = topologicalSort' g (vs ++ outboundVertices v g) (sorted ++ [v])
-
--------- HotDrink functions --------
-
 isVariable :: VertexType -> Bool
 isVariable (VertexVar _) = True
 isVariable _             = False
