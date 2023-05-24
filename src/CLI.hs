33b829926fc50d7afbad78f4984eba2b5457be66
Bo Aanes <boaanes@gmail.com>
2023-05-24 15:05:01 +0200

    command rename 2.0

--- src/CLI.hs
+++ src/CLI.hs
@@ -170,7 +170,7 @@ processInput mode input = do
         ["normal"] -> do
             comps <- gets components
             satisfyInter ((show . identifier . head) comps) >> putLnIO "Entering normal mode" >> return Normal
-        ["addComp"] -> do
+        ["new", "comp"] -> do
             comps <- gets components
             if null comps
                 then modify $ \s -> s { components = components s ++ [Component (length (components s)) Map.empty [] []] }
@@ -180,7 +180,7 @@ processInput mode input = do
                     satisfyInter $ (show . identifier . head) comps
             putLnIO "Added component"
             return mode
-        ["list", nCompsStr] -> do
+        ["new", "list", nCompsStr] -> do
             comps <- gets components
             if null comps
                 then case readMaybe @Int nCompsStr of
@@ -191,7 +191,7 @@ processInput mode input = do
                     _ -> putLnIO "Couldnt parse the number of components"
                 else putLnIO "There are already components defined"
             return mode
-        ["addVar", var, val] -> do
+        ["new", "var", var, val] -> do
             case readValue val of
                 Nothing -> putLnIO "Couldnt parse the value"
                 Just v -> do
@@ -200,7 +200,7 @@ processInput mode input = do
                     modify $ \cs -> cs { components = newComps }
                     putLnIO $ "Added variable: " ++ var ++ " = " ++ val
             return mode
-        ["addConstr", nMethodsStr] -> do
+        ["new", "ctrn", nMethodsStr] -> do
             case readMaybe @Int nMethodsStr of
                 Just n -> do
                     methodGraphs <- liftIO $ traverse (const inputMethod) [1..n]
@@ -210,7 +210,7 @@ processInput mode input = do
                     putLnIO $ "Added constraint with " ++ nMethodsStr ++ " methods"
                 _ -> putLnIO "Couldnt parse the id or the number of methods"
             return mode
-        ["addInter", nMethodsStr] -> do
+        ["new", "ictrn", nMethodsStr] -> do
             case readMaybe @Int nMethodsStr of
                 Just n -> do
                     methodGraphs <- liftIO $ traverse (const inputMethod) [1..n]
@@ -220,7 +220,7 @@ processInput mode input = do
                     putLnIO $ "Added intercalating constraint with " ++ nMethodsStr ++ " methods"
                 _ -> putLnIO "Couldnt parse the id or the number of methods"
             return mode
-        ["updateVar", ident, var, val] -> do
+        ["upd", "var", ident, var, val] -> do
             case readValue val of
                 Just v -> do
                     maybeComp <- findComponent ident
@@ -233,13 +233,13 @@ processInput mode input = do
                             putLnIO $ "Updated variable: " ++ var ++ " = " ++ val
                 _ -> putLnIO "Couldnt parse id or the value"
             return mode
-        ["deleteVar", var] -> do
+        ["del", "var", var] -> do
             comps <- gets components
             let newComps = deleteVariableFromComponent var <$> comps
             modify $ \cs -> cs { components = newComps }
             putLnIO $ "Deleted variable: '" ++ var ++ "' from all components"
             return mode
-        ["insertAfter", ident] -> do
+        ["ins", "after", ident] -> do
             maybePrecedingComp <- findComponent ident
             case maybePrecedingComp of
                 Nothing -> putLnIO "Couldnt find component"
@@ -263,7 +263,7 @@ processInput mode input = do
                     unless (mode == Manual) $ maybe (return ()) satisfyInter ((getPrev compA comps <&> (show . identifier)) <|> Just (show $ identifier compB))
                 _ -> putLnIO "Couldnt find one or both components"
             return mode
-        ["remove", ident] -> do
+        ["rmv", ident] -> do
             maybeComp <- findComponent ident
             case maybeComp of
                 Nothing -> putLnIO "Couldnt find component"
@@ -285,63 +285,69 @@ processInput mode input = do
                 Nothing -> putLnIO "Couldnt find component"
                 Just c  -> putLnIO $ showVariablesOfComponent c
             return mode
-        ["show", "constr"] -> do
+        ["show", "ctrn"] -> do
             comps <- gets components
             let comp = safeHead comps
             case comp of
                 Just c -> putLnIO $ showConstraintsOfComponent c
                 _      -> putLnIO "No components defined"
             return mode
-        ["show", "inter"] -> do
+        ["show", "ictrn"] -> do
             cs <- gets intercalatingConstraints
             putLnIO $ intercalate "\n" $ fmap ((<> "\n" <> replicate 80 '-'). prettyPrintConstraint) cs
             return mode
-        ["show", "strength", ident] -> do
+        ["show", "str", ident] -> do
             maybeComp <- findComponent ident
             case maybeComp of
                 (Just c) -> putLnIO $ showStrengthOfComponent c
                 _        -> putLnIO "Couldnt find component"
             return mode
-        ["show", "plan", ident] -> do
+        ["show", "pln", ident] -> do
             maybeComp <- findComponent ident
             case maybeComp of
                 (Just c) -> putLnIO $ showPlanOfComponent c
                 _        -> putLnIO "Couldnt find component"
             return mode
-        ["runLocal", ident] -> do
+        ["run", "local", ident] -> do
             maybeComp <- findComponent ident
             case maybeComp of
                 (Just c) -> satisfy c
                 _        -> putLnIO "Couldnt find component"
             return mode
-        ["runInter", "inter", ident] -> satisfyInter ident >> return mode
-        ["satisty"] -> gets components >>= \comps -> satisfyInter ((show . identifier . head) comps) >> return mode
+        ["run", "inter", ident] -> satisfyInter ident >> return mode
+        ["run", "all"] -> gets components >>= \comps -> satisfyInter ((show . identifier . head) comps) >> return mode
         ["help"] -> do
             putLnIO $ "\ESC[1;31mYou are in " <> show mode <> " mode\ESC[0m"
             putLnIO "\ESC[31mAvailable commands are:\ESC[0m"
-            putLnIO "manual - enter manual mode, operations will not automatically satisfy the constraint system"
-            putLnIO "normal - enter normal mode, operations will automatically satisfy the constraint system"
-            putLnIO "addComp - add a component"
-            putLnIO "list <n> - add n components"
-            putLnIO "addVar <var> <val> - add a variable to all components"
-            putLnIO "addConstr <n> - add a constraint with n methods to all components"
-            putLnIO "addInter <n> - add an intercalating constraint with n methods"
-            putLnIO "updateVar <id> <var> <val> - update a variable of a component"
-            putLnIO "deleteVar <var> - delete a variable from all components"
-            putLnIO "insertAfter <id> - insert a component after the component with the given id"
-            putLnIO "swap <id> <id> - swap the positions two components"
-            putLnIO "remove <id> - delete a component"
-            putLnIO "show comp - show all components"
-            putLnIO "show var <id> - show all variables of a component (all components have the same set of variables)"
-            putLnIO "show constr - show the constraints of each component"
-            putLnIO "show inter - show all intercalating constraints"
-            putLnIO "show strength <id> - show the strength of the variables of a component"
-            putLnIO "show plan <id> - show the current plan of a component"
-            putLnIO "runLocal <id> - enforce the plan of a component"
-            putLnIO "runInter <id> - satisfy the whole constraint system from the given component to the end"
-            putLnIO "satisfy - satisfy the whole constraint system from the first component to the end"
-            putLnIO "help - show this message"
-            putLnIO "exit - exit the program"
+            putLnIO "manual                     enter manual mode, operations will not automatically satisfy the constraint system"
+            putLnIO "normal                     enter normal mode, operations will automatically satisfy the constraint system"
+            putLnIO ""
+            putLnIO "new comp                   add a component"
+            putLnIO "new list <n>               add n components"
+            putLnIO "new var <var> <val>        add a variable to all components"
+            putLnIO "new ctrn <n>               add a constraint with n methods to all components"
+            putLnIO "new ictrn <n>              add an intercalating constraint with n methods"
+            putLnIO ""
+            putLnIO "upd var <id> <var> <val>   update a variable of a component"
+            putLnIO "del var <var>              delete a variable from all components"
+            putLnIO ""
+            putLnIO "ins after <id>             insert a component after the component with the given id"
+            putLnIO "swap <id> <id>             swap the positions two components"
+            putLnIO "rmv <id>                   remove a component"
+            putLnIO ""
+            putLnIO "show comp                  show all components"
+            putLnIO "show var <id>              show all variables of a component (all components have the same set of variables)"
+            putLnIO "show ctrn                  show the constraints of each component"
+            putLnIO "show ictrn                 show all intercalating constraints"
+            putLnIO "show str <id>              show the strength of the variables of a component"
+            putLnIO "show pln <id>              show the current plan of a component"
+            putLnIO ""
+            putLnIO "run local <id>             enforce the plan of a component"
+            putLnIO "run inter <id>             enforce the intercalating constraint with the given id"
+            putLnIO "run all                    satisfy the whole constraint system from the first component to the end"
+            putLnIO ""
+            putLnIO "help                       show this message"
+            putLnIO "exit                       exit the program"
             return mode
         ["exit"] -> return mode
         _ -> putLnIO "Unknown command" >> return mode
